97a32377f27ae6c3738b38d5c6e866c9f9e97679
Donald <donald.isufi@riinvest.net>
2019-04-24 14:53:23 +0200

    test

--- components/Errors.js
+++ components/Errors.js
@@ -7,7 +7,7 @@ StyleSheet
 
 export default function Errors ({error}){
    return(
-            <View>{!!error && error.graphQlErrors.map(({message})=>(<Text>
+            <View>{!!error && error.graphQlErrors.map(({message})=>(<Text style={{color:'red',fontSize:20}}>
                 {message}
             </Text>))}</View>
         )
