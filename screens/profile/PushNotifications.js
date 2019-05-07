aad87a8e07c2c4eb162f8e069205622c599da814
Altin_Vitija <altin.vitija@riinvest.net>
2019-05-07 16:14:14 +0200

    scroll

--- screens/profile/PushNotifications.js
+++ screens/profile/PushNotifications.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
 
 import {View,Text,StyleSheet} from 'react-native';
-
+import { Ionicons } from '@expo/vector-icons';
 
 export default class PushNotifications extends Component{
 render(){
     return(
-<View>
+<View style={styles.container}>
+
+<Ionicons name="md-checkmark-circle" size={32} color="green" />
 
 </View>
 
      );
    }
  }
+ const styles = StyleSheet.create({
+
+    container :{
+        flex:1,
+        justifyContent:"center",
+        alignItems:'center',
+        backgroundColor:'#fff'
+    },
+})
