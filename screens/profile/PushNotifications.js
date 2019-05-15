ecf42c55bccf06453543696f289b01c34f137396
Altin_Vitija <altin.vitija@riinvest.net>
2019-05-15 14:07:55 +0200

    test

--- screens/profile/PushNotifications.js
+++ screens/profile/PushNotifications.js
@@ -1,14 +1,37 @@
 import React, {Component} from 'react';
 
-import {View,Text,StyleSheet} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import {View,Text,StyleSheet,Picker} from 'react-native';
 
 export default class PushNotifications extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            number:"",
+            
+        };
+    }
 render(){
     return(
 <View style={styles.container}>
 
-<Ionicons name="md-checkmark-circle" size={32} color="green" />
+<Text>Chose your notification time in minutes</Text>
+
+
+<Picker style={styles.picker}
+selectedValue={this.state.minutes}
+onVlaueChange={(minutes)=>this.setState({minutes})}
+
+ 
+
+>
+<Picker.Item label="5" value={5}/>
+<Picker.Item label="10" value={10}/>
+<Picker.Item label="15" value={15}/>
+<Picker.Item label="20" value={20}/>
+<Picker.Item label="25" value={25}/>
+<Picker.Item label="30" value={30}/>
+
+</Picker>
 
 </View>
 
@@ -23,4 +46,7 @@ render(){
         alignItems:'center',
         backgroundColor:'#fff'
     },
+    picker:{
+  width:100,
+    }
 })
